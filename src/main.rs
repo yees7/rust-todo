490b88894fe62ecfa6357ff421a88b45caa7159e
yees7 <josiahag7@gmail.com>
2022-10-06 16:58:40 +1100

    Add version and help section

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fs};
+use colored::Colorize;
 
 pub mod todo;
 pub extern crate colored;
@@ -24,22 +25,45 @@ mod finish_todo;
 use finish_todo::finish_todo;
 
 fn main() {
-    // let path = Path::new("data.json");
-
-    // if !path.exists() {
-    //     File::create(&path).unwrap();
-    // }
-
     let args: Vec<String> = env::args().collect();
 
     match &*args[1] {
-        "get" => get_todo(args[2].parse().unwrap()),
-        "list" => get_todo_all(),
-        "new" => new_todo(args[2].clone()),
-        "clear" => {fs::write("data.json", "[]").unwrap();println!("Todo list cleared.")},
-        "remove" => remove_todo(args[2].parse().unwrap()),
-        "tag" => tag_todo(args[2].parse().expect(&format!("Index '{}' invalid", args[2])), args[3].parse().unwrap()),
-        "done" => finish_todo(args[2].parse().unwrap()),
+        "--help" | "-h" => print_help(),
+        "--get" | "-g" => get_todo(args[2].parse().unwrap()),
+        "--list" | "-l" => get_todo_all(),
+        "--new" | "-n" => new_todo(args[2].clone()),
+        "--clear" | "-c" => {fs::write("data.json", "[]").unwrap();println!("Todo list cleared.")},
+        "--remove" | "-r" => remove_todo(args[2].parse().unwrap()),
+        "--tag" | "-t" => tag_todo(args[2].parse().expect(&format!("Index '{}' invalid", args[2])), args[3].parse().unwrap()),
+        "--done" | "-d" => finish_todo(args[2].parse().unwrap()),
+        "--version" | "-v" => println!("rust-todo v{}", env!("CARGO_PKG_VERSION")),
         _ => println!("Command not found"),
     }
+}
+
+fn print_help() {
+    println!("A todo-list CLI tool written in Rust");
+    println!();
+    println!("Usage:");
+    println!("    rtodo [OPTIONS]");
+    println!("    rtodo [OPTIONS] [ARGUMENTS]");
+    println!();
+    println!("Options:");
+    println!("    --help, -h                     Prints this help message");
+    println!("    --get, -g     [INDEX]          Prints todo info at index");
+    println!("    --list, -l                     Prints all todos");
+    println!("    --new, -n     [NAME]           Creates new todo with name");
+    println!("    --clear, -c                    Clears all todos");
+    println!("    --remove, -r  [INDEX]          Removes todo at index");
+    println!("    --tag, -t     [INDEX] [COLOR]  Tags todo at index with color");
+    println!("    --done, d     [INDEX]          Makes todo finished/unfinished");
+    println!("    --version, -v                  Prints version info");
+    println!();
+    println!("Tag Colors:");
+    println!("    {}", "red ⦿".red());
+    println!("    {}", "blue ⦿".blue());
+    println!("    {}", "yellow ⦿".yellow());
+    println!("    {}", "green ⦿".green());
+    println!("    {}", "cyan ⦿".cyan());
+    println!("    {}", "purple, magenta ⦿".purple());
 }
